9a6ed6e31669bad995eb666026432e179fd12f9e
Brian Boehm <bri.boehm@gmail.com>
2020-05-30 22:20:32 +0000

    Automatically backed up by Learn

--- grandma_dup.rb
+++ grandma_dup.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(speak)
-  if speak == "I LOVE YOU GRANDMA!".upcase
+  if speak_to_grandma == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif speak == speak.upcase
     puts "NO, NOT SINCE 1938!"
